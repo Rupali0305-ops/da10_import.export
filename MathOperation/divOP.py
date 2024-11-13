046ef839002a2c1ec45992145b4f1b8c4939b17b
Rupali0305-ops <hatwarr87@gmail.com>
2024-11-13 15:03:35 +0530

    this is example

---
+++ MathOperation/divOP.py
@@ -0,0 +1,3 @@
+def divvalue(app1 = 0, app2 = 0):
+    print('inside div value')
+    return app1/app2
---
+++ MathOperation/mulOP.py
@@ -0,0 +1,3 @@
+def mulvalue(app1 = 0, app2 = 0):
+    print('inside mul value')
+    return app1*app2
